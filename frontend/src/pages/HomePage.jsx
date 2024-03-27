81f1e22dcf91d5354fb59a92de16e0eed1464b94
saleh sheikh <salehsheikh786@gmail.com>
2024-03-28 01:39:49 +0500

    adding design

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { Box, Container ,Tab,Tabs,TabList,TabPanels,TabPanel,Text} from "@chakra-ui/react";
+import React from "react";
+import SignUp from "../components/authentication/SignUp";
+import Login from "../components/authentication/Login";
 
 const HomePage = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl"fontFamily={"sans-serif"} color={"black"}>Talk a Tive</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth='1px' color="black">
+      <Tabs variant='soft-rounded'>
+  <TabList mb='1em'>
+    <Tab width="50%">Login</Tab>
+    <Tab width="50%">SignUp</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+     <Login/>
+    </TabPanel>
+    <TabPanel>
+      <SignUp/>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
