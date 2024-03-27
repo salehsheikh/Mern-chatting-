23ae72f2230103ab57625af0d56ccc00aad06fa8
saleh sheikh <salehsheikh786@gmail.com>
2024-03-28 03:24:46 +0500

    adding user controllers

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,48 @@
+const asyncHandler=require('express-async-handler');
+const generateToken=require('../config/generateToken');
+const User=require('../models/userModel');
+const registerUser= asyncHandler(async(req,res)=>{
+   const{name,email,password,pic} =req.body;
+
+   if(!name || !email || !password ){
+    throw new Error("please enter all the fields");
+   }
+
+const userExists=await User.findOne({email});
+if(userExists){
+    res.status(400);
+    throw new Error("User already exists");
+}
+const user=await User.create({
+    name,email,password,pic,
+});
+if(user){
+res.status(201).json({
+    _id:user._id,
+    name:user.name,
+    email:user.email,
+    pic:user.pic,
+    token:generateToken(user._id),
+});
+}else{
+    res.status(400);
+    throw new Error("Failed to create the user ");
+}
+});
+const authUser= asyncHandler(async(req,res)=>{
+    const{email,password}=req.body;
+    const user=await User.findOne({email});
+    if(user && (await user.matchPassword(password))){
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            pic:user.pic,
+            token:generateToken(user._id),
+        });
+    }else{
+        res.status(401);
+        throw new Error("invalid Email or Password ");
+    }
+})
+module.exports={registerUser, authUser};
