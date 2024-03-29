24151aca66d456a42e0002687b13d7bace5f3e26
saleh sheikh <salehsheikh786@gmail.com>
2024-03-30 02:23:52 +0500

    adding auth middlewara

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel';
+import asyncHandler from "express-async-handler";
+import dotenv from "dotenv";
+dotenv.config();
+const protect=asyncHandler(async(req,res,next)=>{
+let token;
+if(
+    req.headers.authorization && 
+    req.headers.authorization.startsWith("Bearer")
+){
+    try{
+        token=req.headers.authorization.split("")[1];
+        //decodes jwt token id
+        const decode=jwt.verify(token,process.env.JWT_SECRET);
+       req.user=  await User.findById(decode.id).select("-password");
+       next();
+    }catch(error){
+        res.status(401);
+        throw new Error("Not authorized, token failed");
+    }
+}
+if(!token){
+    res.status(401);
+    throw new Error("Not authorized, token failed");
+}
+});
+export {protect};
