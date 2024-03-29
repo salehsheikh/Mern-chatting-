663112f7a5de0cd3633c93e34df7f590c022d4be
saleh sheikh <salehsheikh786@gmail.com>
2024-03-30 02:24:48 +0500

    adding chat routes

---
+++ backend/Routes/chatRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { protect } from '../middleware/authMiddleware';
+import { accessChat, addToGroup, createGroupChat, fetchChats, removeFromGroup, renameGroup } from '../controllers/chatControllers';
+const router =express.Router();
+router.route("/").post(protect,accessChat);
+router.route("/").get(protect,fetchChats);
+router.route("/group").post(protect,createGroupChat);
+router.route("/rename").put(protect,renameGroup);
+router.route("/groupadd").put(protect,addToGroup);
+router.route("/groupremove").put(protect,removeFromGroup);
+export default router;
