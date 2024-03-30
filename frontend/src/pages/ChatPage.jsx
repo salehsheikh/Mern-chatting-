1e28e46f411ec6210856409fb45ee1cc5c589e08
saleh sheikh <salehsheikh786@gmail.com>
2024-03-31 03:46:27 +0500

    updating chatPage

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+
+import { ChatState } from '../context/ChatProvider'
+import { Box } from '@chakra-ui/react';
+import ChatBox from '../components/miscalleaneous/ChatBox';
+import MyChats from '../components/miscalleaneous/MyChats';
+import SideDrawer from '../components/miscalleaneous/SideDrawer';
 
 const ChatPage = () => {
+  const{user}=  ChatState();
   return (
-    <div>
-      
+    <div style={{width:"100%"}}>
+      {user && <SideDrawer/>}
+      <Box display="flex"
+      justifyContent="space-between"
+      width="100%"
+      h="91.5vh"p="10px">
+        {user && <MyChats/>}
+        {user &&<ChatBox/>}
+      </Box>
     </div>
   )
 }
