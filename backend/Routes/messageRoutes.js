f299323cb29f6fb0d33bbb446e3c88fe63c4d567
saleh sheikh <salehsheikh786@gmail.com>
2024-04-02 00:51:43 +0500

    adding message route

---
+++ backend/Routes/messageRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { protect } from '../middleware/authMiddleware.js';
+import { allMessages, sendMessage } from '../controllers/messageControllers.js';
+const router=express.Router();
+router.route('/').post(protect,sendMessage)
+router.route('/:chatId').get(protect,allMessages)
+export default router;
