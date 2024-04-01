5aa3c16c7b0732adf79ba93bb8683cd5ed81dbf0
saleh sheikh <salehsheikh786@gmail.com>
2024-04-02 00:52:43 +0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
-const express= require('express');
-const { chats } = require('./data/data');
-const dotenv=require('dotenv');
+import express from 'express'
+// import { chats } from'./data/data';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from'./config/db.js';
+import userRoutes from'./Routes/userRoutes.js';
+import chatRoutes from './Routes/chatRoutes.js';
+import messageRoutes from './Routes/messageRoutes.js'
+import {errorHandler,notFound} from'./middleware/errorMidleware.js';
 const app=express();
+app.use(cors());
+app.use(express.json());
+connectDB();
 dotenv.config();
 app.get('/',(req,res)=>{
 res.send("Api is running successfully")
 });
-app.get('/api/chat',(req,res)=>{
-    res.send(chats);
-});
-app.get('/api/chat/:id',(req,res)=>{
-    // console.log(req.params.id);
-    const singleChat=chats.find(c=>c._id=req.params.id);
-    res.send(singleChat);
-})
+app.use("/api/user", userRoutes);
+app.use("/api/chat",chatRoutes);
+app.use("/api/message",messageRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 const PORT=process.env.PORT||5000;
 app.listen(5000,console.log(`server started on port ${PORT}`))
