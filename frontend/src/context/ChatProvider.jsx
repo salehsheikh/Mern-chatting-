765b9550d804d58ca4e0385c00ad83a3305fe012
saleh sheikh <salehsheikh786@gmail.com>
2024-04-04 05:10:42 +0500

    updated context with new states

--- frontend/src/context/ChatProvider.jsx
+++ frontend/src/context/ChatProvider.jsx
@@ -2,27 +2,40 @@ import { useEffect, useState } from "react";
 import { createContext, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
-const ChatContext=createContext();
-const ChatProvider=({children})=>{
-    const navigate=useNavigate();
-    const [user, setUser] = useState();
-    const [selectedChat, setSelectedChat] = useState();
-    const [chats, setChats] = useState([]);
-    
-    useEffect(()=>{
-        const userInfo=JSON.parse(localStorage.getItem("UserInfo"));
-        setUser(userInfo);
-        console.log(userInfo);
-        if(!userInfo){
-            navigate("/")
-        }
-    },[navigate]);
-    return(
-        <ChatContext.Provider value={{user,setUser,selectedChat, setSelectedChat,chats, setChats}}>{children}</ChatContext.Provider>
-    )
+const ChatContext = createContext();
+const ChatProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState();
+  const [selectedChat, setSelectedChat] = useState();
+  const [chats, setChats] = useState([]);
+  const [notification, setNotification] = useState([]);
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("UserInfo"));
+    setUser(userInfo);
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [navigate]);
+  return (
+    <ChatContext.Provider
+      value={{
+        user,
+        setUser,
+        selectedChat,
+        setSelectedChat,
+        chats,
+        setChats,
+        notification,
+        setNotification,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
 };
-export const ChatState=()=>{
-    return useContext(ChatContext);
-}
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
 
 export default ChatProvider;
